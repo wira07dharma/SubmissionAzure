69fa992e8fa2fad07f7f1106c7e233115d55584d
wira07dharma <48935247+wira07dharma@users.noreply.github.com>
2019-06-19 11:15:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 				<a class="nav-link" href="https://submissiondua.azurewebsites.net/">Home</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="https://submissiondua.azurewebsites.net/analyze.html">Analisis Kendaraan<span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="https://submissiondua.azurewebsites.net/analyze.php">Analisis Kendaraan<span class="sr-only">(current)</span></a>
 			</li>
 		</div>
 		</nav>
